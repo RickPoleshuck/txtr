b883db1d4c82e18d6b6a5d482e4a2f7a502a97f5
Rick Poleshuck <RickPoleshuck@gmail.com>
2023-08-21 09:59:04 -0400

    Finish sent properly

--- txtr_dsk/lib/src/views/message/message_view.dart
+++ txtr_dsk/lib/src/views/message/message_view.dart
@@ -23,86 +23,94 @@ class MessageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => MessageBloc()
+      create: (context) =>
+      MessageBloc()
         ..add(MessageLoadedEvent(context, TxtrMessageDTO.empty())),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('TXTR Message'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                context.push(SettingsView.routeName);
-              },
-            ),
-          ],
-        ),
-        body: BlocBuilder<MessageBloc, MessageState>(
-          builder: (context, state) {
-            switch (state) {
-              case MessageSendingState():
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              case MessageSentState():
-                context.pop();
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              case MessageErrorState():
-                return Center(
-                  child: Text(state.error),
-                );
-              case MessageLoadedState():
-                return FormBuilder(
-                    key: _formKey,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    child: Column(
-                      children: [
-                        FormBuilderTextField(
-                          name: 'name',
-                          validator: FormBuilderValidators.match('^.{4,}\$',
-                              errorText: 'Requires at least 4 characters'),
-                          decoration: const TxtrInputDecoration('Name'),
-                          initialValue: contact.name,
-                        ),
-                        FormBuilderTextField(
-                          name: 'address',
-                          validator: FormBuilderValidators.match('^.{4,}\$',
-                              errorText: 'Requires at least 4 characters'),
-                          decoration: const TxtrInputDecoration('Address'),
-                          initialValue: contact.phones[0].number,
-                        ),
-                        FormBuilderTextField(
-                          name: 'body',
-                          maxLines: 6,
-                          validator: FormBuilderValidators.match('^.{1,}\$\$',
-                              errorText: 'Cannot be empty'),
-                          decoration: const TxtrInputDecoration('Message'),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            ElevatedButton(
-                                onPressed: () {
-                                  _formKey.currentState!.save();
-                                  final formData = _formKey.currentState!.value;
-                                  context.read<MessageBloc>().add(
-                                      MessageSendEvent(
-                                          TxtrMessageDTO.fromJson(formData)));
-                                },
-                                child: const Text('Send')),
-                            ElevatedButton(
-                                onPressed: () {
-                                  _formKey.currentState!.reset();
-                                },
-                                child: const Text('Reset')),
-                          ],
-                        )
-                      ],
-                    ));
-            }
-          },
+      child: BlocListener<MessageBloc, MessageState>(
+        listener: (context, state) {
+          if (state is MessageSentState) {
+            context.pop();
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('TXTR Message'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  context.push(SettingsView.routeName);
+                },
+              ),
+            ],
+          ),
+          body: BlocBuilder<MessageBloc, MessageState>(
+            builder: (context, state) {
+              switch (state) {
+                case MessageSendingState():
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case MessageSentState():
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case MessageErrorState():
+                  return Center(
+                    child: Text(state.error),
+                  );
+                case MessageLoadedState():
+                  return FormBuilder(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      child: Column(
+                        children: [
+                          FormBuilderTextField(
+                            name: 'name',
+                            validator: FormBuilderValidators.match('^.{4,}\$',
+                                errorText: 'Requires at least 4 characters'),
+                            decoration: const TxtrInputDecoration('Name'),
+                            initialValue: contact.name,
+                          ),
+                          FormBuilderTextField(
+                            name: 'address',
+                            validator: FormBuilderValidators.match('^.{4,}\$',
+                                errorText: 'Requires at least 4 characters'),
+                            decoration: const TxtrInputDecoration('Address'),
+                            initialValue: contact.phones[0].number,
+                          ),
+                          FormBuilderTextField(
+                            name: 'body',
+                            maxLines: 6,
+                            validator: FormBuilderValidators.match('^.{1,}\$\$',
+                                errorText: 'Cannot be empty'),
+                            decoration: const TxtrInputDecoration('Message'),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {
+                                    _formKey.currentState!.save();
+                                    final formData = _formKey.currentState!
+                                        .value;
+                                    context.read<MessageBloc>().add(
+                                        MessageSendEvent(
+                                            TxtrMessageDTO.fromJson(formData)));
+                                  },
+                                  child: const Text('Send')),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    _formKey.currentState!.reset();
+                                  },
+                                  child: const Text('Reset')),
+                            ],
+                          )
+                        ],
+                      ));
+              }
+            },
+          ),
         ),
       ),
     );
